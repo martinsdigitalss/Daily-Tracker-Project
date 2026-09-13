800640db8600bbc44f634e08546fc998c2272aca
Martins Olowookere <martinsdigitalss@gmail.com>
2026-09-13 19:39:36 +0100

    corrected the base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-base: '/daily-work-tracker/',
+base: '/daily-tracker-project/',
 })
